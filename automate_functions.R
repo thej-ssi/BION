329bd75f61945b0fac56e6a401f9a0b6c83f9e08
thej-ssi <thej@ssi.dk>
2018-09-12 17:05:13 +0200

    Update automate_functions.R

--- automate_functions.R
+++ automate_functions.R
@@ -723,7 +723,7 @@ run_all <- function(rare_prokaryot,rare_eukaryot,rare_fungi,summary_list) {
     filename = paste0(output_dir,"/Fig_9-",i,"_abundance_fungi_individualtop10_",metadata_split_groups[i],".png")
     export(p,file=filename)
   }
-  top_taxa_heatmap(rare_prokaryot,get_top_n_taxa(rare_prokaryot,25),paste0(output_dir,"/prokaryot_heatmap")
+  top_taxa_heatmap(rare_prokaryot,get_top_n_taxa(rare_prokaryot,25),paste0("prokaryot_heatmap")
   top_taxa_heatmap(rare_eukaryot,get_top_n_taxa(rare_eukaryot,25),paste0(output_dir,"/eukaryot_heatmap")
   top_taxa_heatmap(rare_fungi,get_top_n_taxa(rare_fungi,25),paste0(output_dir,"/fungi_heatmap")
   
