855a99f1c19948ed765bbab9739ffcd5235f9603
thej-ssi <thej@ssi.dk>
2018-10-25 15:28:55 +0200

    fix violin

--- automate_functions.R
+++ automate_functions.R
@@ -905,26 +905,28 @@ make_heatmap_object <- function(po,top_10_taxa,variable_name,plot_title,color_li
   return(return_object)
 }
 
+
 make_violin_object <- function(po,variable_name,taxa,plot_title,color_list) {
   color_vector = setup_color_vector(po,variable_name,color_list)[[1]]
   otu = otu_table(po)[taxa,]
   tax_vector = get_taxa_names(po,taxa)
-  variable_vector = get_variable(po,variable_name)
+  tax_vector = taxa
+  print(taxa)
+  variable_vector = as.vector(get_variable(po,variable_name))
   data = rbind(otu,variable_vector)
   data = as.data.frame(t(data))
-  len = ncol(data)
   colnames(data) = c(tax_vector,"Group")
   data2 = melt.data.frame(data,id.vars = "Group")
-  data2$value = as.numeric(data2$value)
+  data2$value = as.numeric(as.vector(data2$value))
   print(color_vector)
-  p <- ggplot(data2[which(!is.na(data2$value) & !is.na(data2$Group)),], aes(x=variable, y=value, fill = Group)) + 
+  p <- ggplot(data2, aes(x=variable, y=value, fill = Group)) + 
     geom_violin() + 
     coord_flip() +
     scale_y_log10() +
     scale_fill_manual(variable_name,values = color_vector) +
     labs(y = "Rarefied sequence counts", x = "Taxa", title = plot_title)
   p
-  return(p)
+  return(list(p,data,data2))
 }
 
 make_taxa_comparison_object <- function(po,variable_name,p_adjust_method) {
